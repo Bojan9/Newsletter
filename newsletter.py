75d6022d3f6a4a16ff0cc9e0872dd984255741e7
Bojan <Bojan9@users.noreply.github.com>
2024-03-22 16:01:28 +0100

    Update newsletter.py

--- newsletter.py
+++ newsletter.py
@@ -10,23 +10,23 @@ load_dotenv()
 gmail_address = os.getenv("GMAIL_ADDRESS")
 gmail_password = os.getenv("GMAIL_PASSWORD")
 
-email = EmailMessage()
-
-email['Subject'] = "Test"
-email["From"] = gmail_address
-
 with open("newsletter_template.html", "r", encoding="utf-8") as file:
     html_content = file.read()
-
-email.add_alternative(html_content, subtype="html")
 
 with open("emails.txt", "r") as file:
     subscriber_email_addresses = [line.strip() for line in file]
 
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context()) as smtp_server:
-    smtp_server.login(gmail_address, gmail_password)
+for subscriber_email_address in subscriber_email_addresses:
+    email = EmailMessage()
+    email['Subject'] = "Test"
+    email["From"] = gmail_address
+    email["To"] = subscriber_email_address
+    email.set_content(html_content, subtype="html")
 
-    for subsciber_email_address in subscriber_email_addresses:
-        email["To"] = subsciber_email_address
-
-        smtp_server.send_message(email)
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context()) as smtp_server:
+            smtp_server.login(gmail_address, gmail_password)
+            smtp_server.send_message(email)
+        print(f"Email sent successfully to {subscriber_email_address}")
+    except Exception as e:
+        print(f"Failed to send email to {subscriber_email_address}. Error: {e}")
